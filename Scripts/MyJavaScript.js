3dc58827a4ab76675d670191efc7859ad4f47fba
etguinn <43616276+etguinn@users.noreply.github.com>
2018-10-12 15:05:53 -0600

    Add files via upload

---
+++ Scripts/MyJavaScript.js
@@ -0,0 +1,14 @@
+﻿//Team 1-3
+//JavaScript file
+
+function w3_open() {
+    document.getElementById("main").style.marginLeft = "25%";
+    document.getElementById("mySidebar").style.width = "25%";
+    document.getElementById("mySidebar").style.display = "block";
+    document.getElementById("openNav").style.display = 'none';
+}
+function w3_close() {
+    document.getElementById("main").style.marginLeft = "0%";
+    document.getElementById("mySidebar").style.display = "none";
+    document.getElementById("openNav").style.display = "inline-block";
+}
